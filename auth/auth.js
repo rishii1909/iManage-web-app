443bffb3fa281cd9fe097d746386e05bd5332b10
Hrushikesh Chapke <hrushikeshchapke@gmail.com>
2022-01-24 13:40:24 +0530

    Email check on register

--- auth/auth.js
+++ auth/auth.js
@@ -20,98 +20,105 @@ passport.use(
     },
     async (req, email, password, done) => {
         try {
-            if(!req.body.team_secret){
-                var user = await UserModel.create(req.body);
-                let teamData = {};
-                teamData.name = `${user.name.trim().split(" ")[0]}'s team`;
-                teamData.root = user._id;
-                teamData.level = 0;
-                teamData.capacity = 1;
-                teamData.users = [user._id]
-                const team = await TeamModel.create(teamData);
-                await UserModel.updateOne({
-                    _id: user._id
-                }, {
-                    team_id: team._id
-                },
-                (err) => {
-                   if(err){
-                       console.log(`Error: ` + err)
-                   }
-                });
-                
-                const template = await NotificationTemplateModel.create({
-                    name : "iManage Default template",
-                    header : "<%Monitor%> Alert",
-                    body : "Monitor status has changed to <%Status%>.\n Advisory to check the resource. \n<%EventDT%>"
-                });
-                console.log(template);
-                if(template){
-                    console.log("here", user);
-                    UserModel.findOneAndUpdate({
-                        _id: user._id,
-                    }, {
-                        $push : { notification_templates : template._id.toString()},
-                    },
-                    (err, doc) => {
-                        if(err) console.log(err);
-                    });
-                }
 
-                console.log(team._id)
-                await TeamModel.findByIdAndUpdate(
-                    {_id : team._id}, 
-                    {
-                        $push : { [`user_agents.${user._id}`] : cloud_agent_id },
-                        $inc : { agent_occupancy : 1 }
-                    },
-                    )
-                req.body.created_data = {
-                    user : user,
-                    team : team
-                }
-                return done(null, {
-                    user : user,
-                    team : team
-                });
-            }else{
-                // Using Team secret
+            UserModel.findOne({
+                email: req.body.email,
+            }).then( async (user) => {
+                if(user) return handle_error("An account for this email already exists.")
 
-                TeamSecretModel.findOne({
-                    secret: req.body.team_secret,
-                }).then(async (secret) => {
-                    if(!secret){
-                        console.log(secret);
-                        return done("Your team secret is invalid.");
-                    }
-                    var user = await UserModel.create({...req.body, ...{team_id : secret.team_id}});
-                    console.log("secret team id", secret.team_id)
-                    TeamModel.findOneAndUpdate({
-                        _id: secret.team_id,
+                if(!req.body.team_secret){
+                    var user = await UserModel.create(req.body);
+                    let teamData = {};
+                    teamData.name = `${user.name.trim().split(" ")[0]}'s team`;
+                    teamData.root = user._id;
+                    teamData.level = 0;
+                    teamData.capacity = 1;
+                    teamData.users = [user._id]
+                    const team = await TeamModel.create(teamData);
+                    await UserModel.updateOne({
+                        _id: user._id
                     }, {
-                        $push : {
-                            users : user._id,
-                        },
-                        $push : { [`user_agents.${user._id}`] : cloud_agent_id },
-                        $inc : { user_occupancy : 1 }
-                    }, (err, team) => {
-                        console.log(team._id);
-                        if (err) {
-                            console.log(`Error: ` + err)
-                        } else {
-                            req.body.created_data = {
-                                user : user,
-                                team : team
-                            }
-                            return done(null, {
-                                user : user,
-                                team : team
-                            });
-                        }
+                        team_id: team._id
+                    },
+                    (err) => {
+                       if(err){
+                           console.log(`Error: ` + err)
+                       }
                     });
                     
-                });
-            }
+                    const template = await NotificationTemplateModel.create({
+                        name : "iManage Default template",
+                        header : "<%Monitor%> Alert",
+                        body : "Monitor status has changed to <%Status%>.\n Advisory to check the resource. \n<%EventDT%>"
+                    });
+                    console.log(template);
+                    if(template){
+                        UserModel.findOneAndUpdate({
+                            _id: user._id,
+                        }, {
+                            $push : { notification_templates : template._id.toString()},
+                        },
+                        (err, doc) => {
+                            if(err) console.log(err);
+                        });
+                    }
+    
+                    await TeamModel.findByIdAndUpdate(
+                        {_id : team._id}, 
+                        {
+                            $push : { [`user_agents.${user._id}`] : cloud_agent_id },
+                            $inc : { agent_occupancy : 1 }
+                        },
+                        )
+                    req.body.created_data = {
+                        user : user,
+                        team : team
+                    }
+                    return done(null, {
+                        user : user,
+                        team : team
+                    });
+                }else{
+                    // Using Team secret
+    
+                    TeamSecretModel.findOne({
+                        secret: req.body.team_secret,
+                    }).then(async (secret) => {
+                        if(!secret){
+                            console.log(secret);
+                            return done("Your team secret is invalid.");
+                        }
+                        var user = await UserModel.create({...req.body, ...{team_id : secret.team_id}});
+                        console.log("secret team id", secret.team_id)
+                        TeamModel.findOneAndUpdate({
+                            _id: secret.team_id,
+                        }, {
+                            $push : {
+                                users : user._id,
+                            },
+                            $push : { [`user_agents.${user._id}`] : cloud_agent_id },
+                            $inc : { user_occupancy : 1 }
+                        }, (err, team) => {
+                            console.log(team._id);
+                            if (err) {
+                                console.log(`Error: ` + err)
+                            } else {
+                                req.body.created_data = {
+                                    user : user,
+                                    team : team
+                                }
+                                return done(null, {
+                                    user : user,
+                                    team : team
+                                });
+                            }
+                        });
+                        
+                    });
+                }
+
+            });
+            
             
         } catch (err) {
             console.log(err);
